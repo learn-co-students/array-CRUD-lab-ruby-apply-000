2ccd1b65407847b8642d0b995dc9e1be1e0b510f
Ashley Robinson <ashleyhowerobinson@gmail.com>
2016-10-12 21:46:26 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,12 +4,10 @@ end
 
 def create_an_array
   ["hello", "my", "old", "friend"]
-
 end
 
 def add_element_to_end_of_array(array, element)
   array << element
-  array
 end
 
 def add_element_to_start_of_array(array, element)
