e9b758684eb5d4eea77315ee35791429aaec12cf
Sebastian  Darena <sdseb2582@gmail.com>
2018-07-27 18:29:49 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,7 +5,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Andy","Katy","John","Rachel"]
+ name =["Andy","Katy","John","Rachel"]
 end
 
 def add_element_to_end_of_array(array, element)
