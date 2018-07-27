7efc07c31935685c60298fb837d1258cb51dbddc
Sebastian  Darena <sdseb2582@gmail.com>
2018-07-27 18:38:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  name[1,2] #
+  name[1,2] # ["Katy", "John"]
+  
 end
 
 def add_element_to_start_of_array(array, element)
