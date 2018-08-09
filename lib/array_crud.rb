7aaf64540e847cba24b0f5fbac99c7bfaef75b11
Pauline Baker <pbaker@hotmail.co.uk>
2018-08-09 14:43:06 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
+
 def create_an_array
-  
+  ['','','','']
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  array.insert(-1, element)
+  end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.insert(0, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.delete_at(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.fetch(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.fetch(0)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.fetch(-1)
 end
