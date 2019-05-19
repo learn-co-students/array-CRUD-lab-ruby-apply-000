a971d388b3eaee9222c85c185d5ac24e80204df4
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-19 10:59:30 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array[]
 end
 
-def create_an_array
-  
+def
+  create_an_array = ["Minh", "Hoang", "Nam", "Namek"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+# add_element_to_end_of_array(array, element) create_an_array.push("My") 
+def 
+  create_an_array << "Mi"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+# add_element_to_start_of_array(array, element)
+def 
+  create_an_array.unsfift("Hiep")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  namek = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  hiep = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  create_an_array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  create_an_array[-1]
 end
