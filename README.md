77e792018f4105e5e8d33fd5e0c91b3817fb905f
jeremy perry <jeremy.m.perry@gmail.com>
2019-07-05 07:23:53 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Array Lab: Create, Retrieve, Update, Delete
+rn # Array Lab: Create, Retrieve, Update, Delete
 
 ## Overview
 
--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+  ["Box", "Cabinet", "Drawer", "Closet"]
 end
 
 def add_element_to_end_of_array(array, element)
+  ["Box", "Cabinet", "Drawer", "Closet", "arrays!"]
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift ("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
+  last_item = array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  first_item = array.shift 
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  array [2]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  first_item = array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  last_item = array[5]
 end
