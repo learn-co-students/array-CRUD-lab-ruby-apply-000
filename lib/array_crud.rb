f0236f010b99e5fdf7fb91bc3041fbaffe423eb6
Ryan May <ryan.m.may@outlook.com>
2019-07-09 13:03:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,4 @@
+
 def create_an_empty_array
   []
 end
@@ -7,36 +8,43 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  a = ["0", "1", "2", "3"] 
-  a << "arrays!"
+  #a = ["0", "1", "2", "3"] 
+  #a << "arrays!"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  a = ["0", "1", "2", "arrays!"]
-  a.unshift "wow"
+  #a = ["0", "1", "2", "arrays!"]
+  #a.unshift "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  a = ["wow", "1", "2", "arrays!"]
-  a.pop
+  #a = ["wow", "1", "2", "arrays!"]
+  #a.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  a = ["wow", "1", "2", "arrays!"]
-  a.shift
+  #a = ["wow", "1", "2", "arrays!"]
+  #a.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  a = ["wow", "1", "am", "arrays!"]
-  a[2]
+  #a = ["wow", "1", "am", "arrays!"]
+  #a[2]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  a = ["wow", "1", "am", "arrays!"]
-  a[0]
+  #a = ["wow", "1", "am", "arrays!"]
+  #a[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  a = ["wow", "1", "am", "arrays!"]
-  a[-1]
+  #a = ["wow", "1", "am", "arrays!"]
+  #a[-1]
+  array[-1]
 end
