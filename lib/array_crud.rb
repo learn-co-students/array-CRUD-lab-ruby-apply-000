856d09300316b453fbdccbae19d4fd805ab3221b
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-14 15:46:30 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  my_array = []
+ my_array = []
 end
 
 def create_an_array
@@ -7,36 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["a", "b", "c", "d"]
-  my_array << "arrays!"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["a", "b", "c", "d"]
-  my_array.unshift("wow")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ["wow","a", "b", "c", "d", "arrays!"]
-  my_array.pop
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  my_array = ["wow","a", "b", "c", "d"]
-  my_array.shift
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array = ["am", "b", "c", "d"]
-  return my_array[0]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  my_array = ["wow", "b", "c", "d"]
-  return my_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  my_array = ["wow", "b", "c", "d", "arrays!"]
-  return my_array[4]
+  array[array.length - 1]
 end
